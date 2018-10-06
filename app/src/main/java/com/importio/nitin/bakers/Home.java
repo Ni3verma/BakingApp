11df974331ef673b69b573b86b30f5c45c770d8d
Nitin <canvas.nv@gmail.com>
2018-10-07 01:00:05 +0530

    get JSON response

--- app/src/main/java/com/importio/nitin/bakers/Home.java
+++ app/src/main/java/com/importio/nitin/bakers/Home.java
@@ -1,7 +1,9 @@
 package com.importio.nitin.bakers;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        GetReceipesAsyncTask task = new GetReceipesAsyncTask();
+        task.execute();
+    }
+
+    private class GetReceipesAsyncTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return NetworkUtils.getJSONResponse();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null)
+                Log.d("Nitin", s.substring(0, 30));
+        }
     }
 }
---
+++ app/src/main/java/com/importio/nitin/bakers/NetworkUtils.java
@@ -0,0 +1,64 @@
+package com.importio.nitin.bakers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetworkUtils {
+
+    public static String getJSONResponse() {
+        URL url = null;
+        try {
+            url = new URL("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String response;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuilder buffer = new StringBuilder();
+            if (inputStream == null)
+                return null;
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+            }
+            if (buffer.length() == 0) {
+                //stream is empty, no point in parsing
+                return null;
+            }
+            response = buffer.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        //Log.d("Nitin","response="+response);
+        return response;
+    }
+}
